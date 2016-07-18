fedeaade519acb0a7fdf32b32985c5a629d3ff43
李中党 <lizhongdang@idcos.com>
2016-07-18 17:57:40 +0800

    merge function

--- app/services/api/device/service.js
+++ app/services/api/device/service.js
@@ -6,25 +6,6 @@ import Ember from 'ember';
 var ajax = Ember.$.ajax;
 
 export default Ember.Service.extend({
-
-    /*
-    * 新增操作系统
-    */
-    create : function(form) {
-        var url = "/api/osinstall/v1/device/add";
-		//生成发请求数据对象
-		var data = {};
-		data = form;
-
-		//发送ajax请求
-        return ajax({
-			'method': 'POST',
-			'contentType': "application/json; charset=utf-8",
-			'url': url,
-			'data': JSON.stringify(data),
-        });
-    },
-
     /*
     * 重装操作系统
     */
@@ -147,24 +128,6 @@ export default Ember.Service.extend({
     */
     getFull : function(id) {
         var url = "/api/osinstall/v1/device/viewFull";
-		//生成发请求数据对象
-		var data = {};
-		data.ID = parseInt(id);
-
-		//发送ajax请求
-        return ajax({
-			'method': 'POST',
-			'contentType': "application/json; charset=utf-8",
-			'url': url,
-			'data': JSON.stringify(data),
-        });
-    },
-
-    /*
-    * 删除操作系统具体信息
-    */
-    deleteRowById : function(id) {
-        var url = "/api/osinstall/v1/device/delete";
 		//生成发请求数据对象
 		var data = {};
 		data.ID = parseInt(id);
