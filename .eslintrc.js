59dc8c6fa199b8dc60380b3b59979c2c7edeead4
chenxuejia <chenxuejia@idcos.com>
2016-12-26 10:37:53 +0800

    jslint 改为 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  globals: {
+      "Ember": true,
+      "swal": true,
+      "go": true,
+      "window": true,
+      "moment": true,
+      "Promise": true,
+      "Uint8Array": true,
+      "JSONTree": true,
+      "$": true,
+      "saveAs": true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    'browser': true
+  },
+  rules: {
+    "no-mixed-spaces-and-tabs": [0],
+    "no-unused-vars": 0,
+    "comma-dangle": 0,
+    "no-console": 0,
+    "no-empty": 0,
+    "no-extra-boolean-cast": 0,
+    "no-case-declarations": 0
+  }
+};
